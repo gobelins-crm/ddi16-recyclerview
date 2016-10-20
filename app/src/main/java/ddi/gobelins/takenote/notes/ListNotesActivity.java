aa1c2be5ba24d091337042b5566bfdb2063b908a
louisbl <louis@beltramo.me>
2016-10-20 13:24:49 +0200

    Add ListNotes activity and fragment

---
+++ app/src/main/java/ddi/gobelins/takenote/notes/ListNotesActivity.java
@@ -0,0 +1,15 @@
+package ddi.gobelins.takenote.notes;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import ddi.gobelins.takenote.R;
+
+public class ListNotesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_notes);
+    }
+}
