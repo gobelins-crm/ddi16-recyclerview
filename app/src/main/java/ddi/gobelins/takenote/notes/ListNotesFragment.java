8df8127ee9d355e3d530b2faba914216b2c60f8b
louisbl <louis@beltramo.me>
2016-10-20 13:28:10 +0200

    Add recyclerview with layout manager

--- app/src/main/java/ddi/gobelins/takenote/notes/ListNotesFragment.java
+++ app/src/main/java/ddi/gobelins/takenote/notes/ListNotesFragment.java
@@ -3,6 +3,8 @@ package ddi.gobelins.takenote.notes;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -26,6 +28,9 @@ public class ListNotesFragment extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_list_notes, container, false);
 
+        RecyclerView recyclerView = (RecyclerView) view.findViewById(R.id.list_notes_recyclerview);
+        recyclerView.setLayoutManager(new LinearLayoutManager(view.getContext()));
+        
         return view;
     }
 
