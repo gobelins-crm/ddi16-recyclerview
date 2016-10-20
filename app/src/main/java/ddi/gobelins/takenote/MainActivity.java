a5381f57ec2c8fae7c816e82f21a333832b9bb79
louisbl <louis@beltramo.me>
2016-10-20 14:37:18 +0200

    Start list notes activity on button1 click

--- app/src/main/java/ddi/gobelins/takenote/MainActivity.java
+++ app/src/main/java/ddi/gobelins/takenote/MainActivity.java
@@ -1,7 +1,12 @@
 package ddi.gobelins.takenote;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import ddi.gobelins.takenote.notes.ListNotesActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button1 = (Button) findViewById(R.id.main_button_1);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, ListNotesActivity.class));
+            }
+        });
     }
 }
