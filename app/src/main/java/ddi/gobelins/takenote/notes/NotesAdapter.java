7d3b9ea43df010a3763e708e1058fcaff4c83ab2
louisbl <louis@beltramo.me>
2016-10-20 13:32:10 +0200

    Create empty notes adapter

---
+++ app/src/main/java/ddi/gobelins/takenote/notes/NotesAdapter.java
@@ -0,0 +1,26 @@
+package ddi.gobelins.takenote.notes;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.ViewGroup;
+
+/**
+ * Created by louisbl on 10/20/16.
+ */
+
+public class NotesAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
