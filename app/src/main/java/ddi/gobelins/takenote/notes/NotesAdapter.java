323b7c9e108d8cbf5a341a3afcd7cdaffcb09010
louisbl <louis@beltramo.me>
2016-10-20 13:35:13 +0200

    Add notes list in notes adapter

--- app/src/main/java/ddi/gobelins/takenote/notes/NotesAdapter.java
+++ app/src/main/java/ddi/gobelins/takenote/notes/NotesAdapter.java
@@ -4,11 +4,18 @@ import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.util.List;
+
 /**
  * Created by louisbl on 10/20/16.
  */
 
 public class NotesAdapter extends RecyclerView.Adapter<NotesAdapter.ViewHolder> {
+    private List<Note> notes;
+
+    public NotesAdapter(List<Note> notes) {
+        this.notes = notes;
+    }
 
     @Override
     public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
@@ -22,7 +29,7 @@ public class NotesAdapter extends RecyclerView.Adapter<NotesAdapter.ViewHolder> 
 
     @Override
     public int getItemCount() {
-        return 0;
+        return notes.size();
     }
 
     static class ViewHolder extends RecyclerView.ViewHolder {
